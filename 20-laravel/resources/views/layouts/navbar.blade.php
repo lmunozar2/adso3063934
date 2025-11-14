5272e0dec3d50bb8cad4a9bbc321b70b4e996314
lmunozar2 <luismik1010@gmail.com>
2025-11-14 17:36:41 -0500

    Actualizacion del dashboard, agregando card para ver los usuario, las mascotas y las adopciones 14/11/2025

--- 20-laravel/resources/views/layouts/navbar.blade.php
+++ 20-laravel/resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="navbar bg-[#354052aa] text-white shadow-sm fixed top-0">
+<div class="navbar bg-[#354052aa] text-white shadow-sm fixed top-0 z-2" >
     <div class="navbar-start">
         <div class="dropdown ">
             <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
@@ -30,6 +30,8 @@
                         Dashboard
                     </a>
                 </li>
+                {{-- Role: Administration --}}
+                @if(Auth::user()->role == 'Administrador') {{-- Se usa para lo que este dentro solo se vea cuando se ingrese con el admin --}}
                 <li>
                     <a href="{{ url('users/') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor"
@@ -63,11 +65,30 @@
                         Module Adoptions
                     </a>
                 </li>
+                @else
+                <li>
+                    <a href="{{ url('myprofile/') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor" viewBox="0 0 256 256"><path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path></svg>
+                        My Profile
+                    </a>
+
+                    <a href="{{ url('myadoptions/') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor" viewBox="0 0 256 256"><path d="M212,80a28,28,0,1,0,28,28A28,28,0,0,0,212,80Zm0,40a12,12,0,1,1,12-12A12,12,0,0,1,212,120ZM72,108a28,28,0,1,0-28,28A28,28,0,0,0,72,108ZM44,120a12,12,0,1,1,12-12A12,12,0,0,1,44,120ZM92,88A28,28,0,1,0,64,60,28,28,0,0,0,92,88Zm0-40A12,12,0,1,1,80,60,12,12,0,0,1,92,48Zm72,40a28,28,0,1,0-28-28A28,28,0,0,0,164,88Zm0-40a12,12,0,1,1-12,12A12,12,0,0,1,164,48Zm23.12,100.86a35.3,35.3,0,0,1-16.87-21.14,44,44,0,0,0-84.5,0A35.25,35.25,0,0,1,69,148.82,40,40,0,0,0,88,224a39.48,39.48,0,0,0,15.52-3.13,64.09,64.09,0,0,1,48.87,0,40,40,0,0,0,34.73-72ZM168,208a24,24,0,0,1-9.45-1.93,80.14,80.14,0,0,0-61.19,0,24,24,0,0,1-20.71-43.26,51.22,51.22,0,0,0,24.46-30.67,28,28,0,0,1,53.78,0,51.27,51.27,0,0,0,24.53,30.71A24,24,0,0,1,168,208Z"></path></svg>
+                        My Adoptions
+                    </a>
+
+                    <a href="{{ url('makeadoptions/') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor" viewBox="0 0 256 256"><path d="M230.33,141.06a24.34,24.34,0,0,0-18.61-4.77C230.5,117.33,240,98.48,240,80c0-26.47-21.29-48-47.46-48A47.58,47.58,0,0,0,156,48.75,47.58,47.58,0,0,0,119.46,32C93.29,32,72,53.53,72,80c0,11,3.24,21.69,10.06,33a31.87,31.87,0,0,0-14.75,8.4L44.69,144H16A16,16,0,0,0,0,160v40a16,16,0,0,0,16,16H120a7.93,7.93,0,0,0,1.94-.24l64-16a6.94,6.94,0,0,0,1.19-.4L226,182.82l.44-.2a24.6,24.6,0,0,0,3.93-41.56ZM119.46,48A31.15,31.15,0,0,1,148.6,67a8,8,0,0,0,14.8,0,31.15,31.15,0,0,1,29.14-19C209.59,48,224,62.65,224,80c0,19.51-15.79,41.58-45.66,63.9l-11.09,2.55A28,28,0,0,0,140,112H100.68C92.05,100.36,88,90.12,88,80,88,62.65,102.41,48,119.46,48ZM16,160H40v40H16Zm203.43,8.21-38,16.18L119,200H56V155.31l22.63-22.62A15.86,15.86,0,0,1,89.94,128H140a12,12,0,0,1,0,24H112a8,8,0,0,0,0,16h32a8.32,8.32,0,0,0,1.79-.2l67-15.41.31-.08a8.6,8.6,0,0,1,6.3,15.9Z"></path></svg>
+                        make Adoptions
+                    </a>
+                </li>
+                @endif
                 <li>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <a href="{{ route('logout') }}" class="flex gap-2" onclick="event.preventDefault();
-        this.closest('form').submit();">
+                        <a href="{{ route('logout') }}" class="flex gap-2 items-center justify-center"
+                         onclick="event.preventDefault();
+                            this.closest('form').submit();">
                             <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor"
                                 viewBox="0 0 256 256">
                                 <path
